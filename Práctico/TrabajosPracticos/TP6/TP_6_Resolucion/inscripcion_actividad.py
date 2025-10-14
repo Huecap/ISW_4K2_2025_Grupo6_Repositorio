52a26a27d2353942b81ad24c4cf434d492477928
juancamadey <jgarciaamadey@gmail.com>
2025-10-14 17:00:21 -0300

    feature/api-flask

--- Práctico/TrabajosPracticos/TP6/TP_6_Resolucion/inscripcion_actividad.py
+++ Práctico/TrabajosPracticos/TP6/TP_6_Resolucion/inscripcion_actividad.py
@@ -1,7 +1,16 @@
 
-def inscribir_visitante(actividad, visitante, horario):
-    if actividad.horario == horario and len(actividad.visitantes) < actividad.cupo_total and visitante.acepta_tyc == True:
-        actividad.visitantes.append(visitante)
-        return True
-    else:
-        return False
+def inscribir_visitante(actividad, nuevos_visitantes, horario):
+  
+    cupo_disponible = actividad.cupo_total - len(actividad.visitantes)
+    if actividad.horario != horario or len(nuevos_visitantes) > cupo_disponible:
+        print(" cupo disponible menor al de la inscripcion")
+        print(f"cupo { cupo_disponible}")
+        return False  # Falla si el horario no coincide o si el grupo no cabe.
+
+    for visitante in nuevos_visitantes:
+        if not visitante.acepta_tyc:
+            print("no acepto los terminos")
+            return False 
+
+    actividad.visitantes.extend(nuevos_visitantes)
+    return True
