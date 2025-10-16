4bcb55c5cd71b5382550b4db661449ed8334d167
Arrigo Gaspar <arrigogaspar0@gmail.com>
2025-10-16 15:27:55 -0300

    fix(tests): Acomodo los distintos archivos de test en uno solo

--- Práctico/TrabajosPracticos/TP6/TP_6_Resolucion/test_inscripcion.py
+++ Práctico/TrabajosPracticos/TP6/TP_6_Resolucion/test_inscripcion.py
@@ -18,7 +18,6 @@ def test_incripcion_exitosa():
     resultado = tirolesa.inscribir_visitante(visitante, "11:00")
     assert resultado == True  # PASA
     
-    
 def test_inscripcion_sin_cupo():
     """Probar inscribirse a una actividad que no tiene cupo para el horario seleccionado"""
     
@@ -45,7 +44,6 @@ def test_inscripcion_sin_cupo():
     )
     resultado = tirolesa.inscribir_visitante(visitante_extra, "11:00")
     assert resultado == False # FALLA
-    
     
 def test_incripcion_sin_vestimenta_no_requerida():
     """Probar inscribirse a una actividad sin ingresar talle de vestimenta 
@@ -94,7 +92,7 @@ def test_incripcion_sin_vestimenta_requerido():
         acepta_tyc=True
     ) 
 
-    )
+    
     # Crear actividad Safari con 2 cupo, e inscribimos a un visitante
     safari = Safari(2, [visitante_existente], "11:00", "Seguir al guia")
     # Probamos inscribir a otro visitante cuando no hay cupo
@@ -103,8 +101,7 @@ def test_incripcion_sin_vestimenta_requerido():
         
     resultado = tirolesa.inscribir_visitante(visitante, "09:00")
     assert resultado == False #FALLA
-    
-    
+     
 def test_inscripcion_sin_aceptar_tyc():
     """
     Probar Inscribir un visitante a una actividad sin aceptar los terminos y condiciones
@@ -120,12 +117,49 @@ def test_inscripcion_sin_aceptar_tyc():
         )
     resultado = safari.inscribir_visitante(visitante=visitante, horario="08:00")
     assert resultado == False # FALLA
+
+def test_inscripcion_multiples_actividades_mismo_horario_con_cupo():
+    from actividad import Safari
+    from actividad import Tirolesa
+    from visitante import Visitante
+    visitante_nuevo = Visitante(
+    nombre="Charlie", 
+    dni="12345678", 
+    edad=35, 
+    talla_vestimenta="m",
+    acepta_tyc=True
+    )
+    # Crear actividad Safari con 2 cupo, e inscribimos a un visitante
+    safari = Safari(2, [], "11:00", "Seguir al guia")
+    tirolesa = Tirolesa(2, [], "11:00", "Seguir al guia")
+    inscripcion_1 = safari.inscribir_visitante(visitante_nuevo, "11:00")
+    inscripcion_2 = tirolesa.inscribir_visitante(visitante_nuevo, "11:00")
+    assert inscripcion_1 == True & inscripcion_2 == False
+
+def test_inscripcion_multiples_actividades_distinto_horario_con_cupo():
+    from actividad import Safari
+    from actividad import Tirolesa
+    from visitante import Visitante
+    visitante_nuevo = Visitante(
+    nombre="Charlie", 
+    dni="12345678", 
+    edad=35, 
+    talla_vestimenta="m",
+    acepta_tyc=True
+    )
+    # Crear actividad Safari con 2 cupo, e inscribimos a un visitante
+    safari = Safari(2, [], "11:00", "Seguir al guia")
+    tirolesa = Tirolesa(2, [], "12:00", "Seguir al guia")
+    inscripcion_1 = safari.inscribir_visitante(visitante_nuevo, "11:00")
+    inscripcion_2 = tirolesa.inscribir_visitante(visitante_nuevo, "12:00")
+    assert inscripcion_1 == True & inscripcion_2 == False
     
 if __name__ == "__main__":
-    
     test_incripcion_exitosa()
     test_inscripcion_sin_cupo()
     test_incripcion_sin_vestimenta_no_requerida()
     test_inscripcion_fuera_horario()
     test_incripcion_sin_vestimenta_requerido()
     test_inscripcion_sin_aceptar_tyc()
+    test_inscripcion_multiples_actividades_mismo_horario_con_cupo()
+    test_inscripcion_multiples_actividades_distinto_horario_con_cupo()
